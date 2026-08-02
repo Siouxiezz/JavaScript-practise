05c2a9367b3df19f885c205dbb064e712c9cae12
Siouxiezz <fou4chik@gmail.com>
2026-08-02 17:50:02 +0300

    Done homework EvenOrOddNumbers

---
+++ EvenOrOddNumb.js
@@ -0,0 +1,9 @@
+let number = Number(prompt("Type a number you want to check"));
+
+if(number % 2 === 0)
+{
+    console.log("Number " + number + " is even");
+}else
+{
+    console.log("Number " + number + " is odd");
+}
